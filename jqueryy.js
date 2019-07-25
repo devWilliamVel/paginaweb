24f1499a23381fe96835c3d4b078fc3bc479555d
William Velasquez <william.velas@gmail.com>
2019-07-25 17:54:24 +0200

    agregue files necesarios
    sirven como base para las pruebas

---
+++ jqueryy.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+	window.onscroll = function(){
+		var div1 = document.getElementById('div1');
+		var div2 = document.getElementById('div2');
+		let div1Top = div1.offsetTop;
+		let div1Bottom = div1Top + div1.offsetHeight;
+		let div2Top = div2.offsetTop;
+		let div2Bottom = div2Top + div2.offsetHeight;
+
+		console.log('DIV 1');
+		console.log('Top: ' + div1Top);
+		console.log('Bottom: ' + div1Bottom);
+		console.log('DIV 2');
+		console.log('Top: ' + div2Top);
+		console.log('Bottom: ' + div2Bottom);
+	}
+});
+
+
+
+
+
+
+http://testwilliam.bpm-power.com/responsive-it/home/action_page.php?firstname=wil&email=wil%40gmail.com&n_or=242526&subject=non+trovo+la+mia+mail
+
+
+<script>
+document.getElementById('').removeAttribute("disabled");
+</script>
